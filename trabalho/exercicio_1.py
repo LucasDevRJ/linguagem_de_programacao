2a559ed0aefd39193794af3de033e602f48506d5
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-12 13:06:44 -0300

    exercicio 1: forma de finalizar programa

--- trabalho/exercicio_1.py
+++ trabalho/exercicio_1.py
@@ -38,29 +38,37 @@ class Calculadora:
     def raiz_quadrada(self):
         return math.sqrt(self.penultimo_numero_ru + self.ultimo_numero_ru)
 
-print("-------------------|MENU|-------------------")
-print("1 - Soma")
-print("2 - Subtração")
-print("3 - Multiplicação")
-print("4 - Divisão")
-print("5 - Elevar")
-print("6 - Resto")
-print("7 - Raiz Quadrada")
-opcao = int(input("Digite a sua operação desejada: "))
-calculadora = Calculadora()
-calculadora.solicitaValores()
+opcao = 0
+while (opcao != 8) :
+    print("-------------------|MENU|-------------------")
+    print("1 - Soma")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+    print("5 - Elevar")
+    print("6 - Resto")
+    print("7 - Raiz Quadrada")
+    print("8 - Sair")
+    opcao = int(input("Digite a sua operação desejada: "))
 
-if (opcao == 1):
-    print(calculadora.get_penultimo_numero_ru(), "+", calculadora.get_ultimo_numero_ru(), "=", calculadora.soma())
-elif (opcao == 2):
-    print(calculadora.get_penultimo_numero_ru(), "-", calculadora.get_ultimo_numero_ru(), "=", calculadora.subtrai())
-elif (opcao == 3):
-    print(calculadora.get_penultimo_numero_ru(), "x", calculadora.get_ultimo_numero_ru(), "=", calculadora.multiplica())
-elif (opcao == 4):
-    print(calculadora.get_penultimo_numero_ru(), "/", calculadora.get_ultimo_numero_ru(), "=", calculadora.dividi())
-elif (opcao == 5):
-    print(calculadora.get_penultimo_numero_ru(), "^", calculadora.get_ultimo_numero_ru(), "=", calculadora.eleva())
-elif (opcao == 6):
-    print(calculadora.get_penultimo_numero_ru(), "%", calculadora.get_ultimo_numero_ru(), "=", calculadora.resto())
-elif (opcao == 7):
-    print("√", calculadora.get_penultimo_numero_ru(), "+", calculadora.get_ultimo_numero_ru(), "=", calculadora.raiz_quadrada())
+    if (opcao != 8):
+        calculadora = Calculadora()
+        calculadora.solicitaValores()
+
+    if (opcao == 1):
+        print(calculadora.get_penultimo_numero_ru(), "+", calculadora.get_ultimo_numero_ru(), "=", calculadora.soma())
+    elif (opcao == 2):
+        print(calculadora.get_penultimo_numero_ru(), "-", calculadora.get_ultimo_numero_ru(), "=", calculadora.subtrai())
+    elif (opcao == 3):
+        print(calculadora.get_penultimo_numero_ru(), "x", calculadora.get_ultimo_numero_ru(), "=", calculadora.multiplica())
+    elif (opcao == 4):
+        print(calculadora.get_penultimo_numero_ru(), "/", calculadora.get_ultimo_numero_ru(), "=", calculadora.dividi())
+    elif (opcao == 5):
+        print(calculadora.get_penultimo_numero_ru(), "^", calculadora.get_ultimo_numero_ru(), "=", calculadora.eleva())
+    elif (opcao == 6):
+        print(calculadora.get_penultimo_numero_ru(), "%", calculadora.get_ultimo_numero_ru(), "=", calculadora.resto())
+    elif (opcao == 7):
+        print("√", calculadora.get_penultimo_numero_ru(), "+", calculadora.get_ultimo_numero_ru(), "=", calculadora.raiz_quadrada())
+    elif (opcao == 8):
+        print("Programa finalizado.")
+    print("--------------------------------------------")
