4b614c86539a14fc2481a4e15474039de592802f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-01 20:51:21 -0300

    criando função para calcular reajuste

--- aula_1-bibliotecas_em_python/salario.py
+++ aula_1-bibliotecas_em_python/salario.py
@@ -6,14 +6,12 @@ salario_lider = 9000
 #reajuste salarial
 reajuste = 0.035
 
-if salario_analista > salario_base:
-    salario_analista *= reajuste
-    print(f'Novo salário: R$ {salario_analista}')
+def reajustar_salario(salario, valor_base, valor_reajuste):
+    if salario > salario_base:
+        salario *= valor_reajuste
+        print(f'Novo salário: R$ {salario}')
+    return salario
 
-if salario_estagiario > salario_base:
-    salario_estagiario *= reajuste
-    print(f'Novo salário: {salario_estagiario}')
-
-if salario_lider > salario_base:
-    salario_lider *= reajuste
-    print(f'Novo salário: {salario_lider}')
+salario_analista = reajustar_salario(salario_analista, salario_base, reajuste)
+salario_estagiario = reajustar_salario(salario_estagiario, salario_base, reajuste)
+salario_lider = reajustar_salario(salario_lider, salario_base, reajuste)
