2166dbbda8eca529c889f9ac247d6f69f0426585
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-08 16:03:34 -0300

    array do tipo zero e one

--- aula_1-bibliotecas_em_python/aula_pratica.py
+++ aula_1-bibliotecas_em_python/aula_pratica.py
@@ -15,5 +15,10 @@ array_elementos = np.arange(numeros_elementos)
 print(array_elementos[-1])
 
 #criar vetor com numpy
-array_teste = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-print(f'criação de array numpy utilizando o método .array: \n{array_teste}\n')
+array_normal = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+array_de_um = np.ones([10])
+array_de_zero = np.zeros([12])
+
+print(f'criação de array numpy utilizando o método .array: {array_normal}')
+print(f'criação de array numpy utilizando o método .ones: {array_de_um}')
+print(f'criação de array numpy utilizando o método .zeros: {array_de_zero}')
