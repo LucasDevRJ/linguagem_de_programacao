0dffc1a9b80b1d088b142b1a84c12853081566b0
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-08 15:50:22 -0300

    diferença entre uma lista comum e uma de numpy

--- aula_1-bibliotecas_em_python/aula_pratica.py
+++ aula_1-bibliotecas_em_python/aula_pratica.py
@@ -1,6 +1,15 @@
-numero_elementos = 100000000
+import numpy as np
+
+#lista normal
+numeros_elementos = 100000000
 lista_elementos = []
-for x in range(numero_elementos):
+for x in range(numeros_elementos):
     lista_elementos.append(x)
 
 print(lista_elementos[-1])
+
+#lista com numpy
+#mais rápida
+#menos consumo de hardware
+array_elementos = np.arange(numeros_elementos)
+print(array_elementos[-1])
