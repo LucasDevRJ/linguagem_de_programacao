877a0299e405bdb7edb727d5c983ee9d2a1d17c4
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-09 19:49:13 -0300

    armazenando o tamanho da coluna

--- aula_2-python_para_machine_learning_e_inteligencia_artificial/aula_pratica.py
+++ aula_2-python_para_machine_learning_e_inteligencia_artificial/aula_pratica.py
@@ -1,3 +1,14 @@
 import pandas as pd
 
 df = pd.read_csv('combustivel.csv', sep=';', encoding='ISO-8859-1')
+dados = df.head()
+print(dados)
+valores_colunas = df.columns.values
+print(valores_colunas)
+df_colunas = ['Regiao - Sigla', 'Estado - Sigla', 'Produto']
+df_filtrado = df.filter(items=df_colunas)
+df_filtrado = df.rename(columns={'Regiao - Sigla': 'Região', 'Estado - Sigla': 'Estado'})
+print(df_filtrado)
+
+colunas_estados = df_filtrado['Estado']
+print(colunas_estados)
