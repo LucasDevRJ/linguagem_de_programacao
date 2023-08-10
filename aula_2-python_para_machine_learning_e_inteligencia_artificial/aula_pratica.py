db5ba0018e3d628a9e56bae5068cef7eb171eb21
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-09 21:53:52 -0300

    categoria dos produtos sendo armazenadas

--- aula_2-python_para_machine_learning_e_inteligencia_artificial/aula_pratica.py
+++ aula_2-python_para_machine_learning_e_inteligencia_artificial/aula_pratica.py
@@ -22,3 +22,20 @@ df_gasolina = df_filtrado.filter(items=df_colunas2)
 print(df_gasolina)
 df_gasolina = df_gasolina.loc[df_gasolina['Produto'] == 'Gasolina']
 print(df_gasolina)
+
+df_gasolina['Valor da Venda'] = df_gasolina['Valor de Venda'].str.replace(',', '.')
+df_gasolina = df_gasolina.astype({'Valor de Venda': 'float32'})
+df_gasolina = df_gasolina.groupby('Estado').mean()
+df_gasolina = df_gasolina.sort_values(by='Valor de Venda', ascending=False)
+print(df_gasolina)
+
+df_colunas3 = ['Estado', 'Produto', 'Valor de Venda']
+df_gasolina3 = df_filtrado.filter(items=df_colunas3)
+regex = '^Gasolina'
+df_gasolina3 = df_gasolina3.loc[df_gasolina3['Produto'].str.contains(regex, regex = True)]
+print(df_gasolina3)
+
+df_gasolina3['Valor de Venda'] = df_gasolina3['Valor de Venda'].str.replace(',', '.')
+df_gasolina3 = df_gasolina3.astype({'Valor de Venda': 'float32'})
+df_gasolina3 = df_gasolina3.groupby('Produto').mean()
+print(df_gasolina3)
