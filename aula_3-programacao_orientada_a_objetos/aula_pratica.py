6ce23a7da9f658f6f2128ed5aa65d786dd7ae622
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-14 18:20:52 -0300

    instanciando classe

--- aula_3-programacao_orientada_a_objetos/aula_pratica.py
+++ aula_3-programacao_orientada_a_objetos/aula_pratica.py
@@ -2,3 +2,14 @@
 print(type("olá"))
 print(type(1))
 print(type(1.5))
+
+#métodos de String
+print("olá".upper())
+
+class Jogador:
+    def jogar(self):
+        print('Método jogar foi inicializado')
+
+
+j1 = Jogador()
+j1.jogar()
