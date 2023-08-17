6b855d70a266dff3475374327e32ca4d18a3bee5
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-17 09:03:36 -0300

    alterações no arquivo utilizando open as

--- aula_4-persistencia_em_arquivo/aula_pratica.py
+++ aula_4-persistencia_em_arquivo/aula_pratica.py
@@ -1,5 +1,5 @@
 arquivo = open('exemplo.txt', 'w+') #w+ cria novo arquivo ou o substitui
-arquivo.write('Ola Mundo!\nEsta e a segunda linha\nEsta e a terceira linha')
+arquivo.write('Ola Mundo!\nEsta e a segunda linha\nEsta e a terceira linha\n')
 arquivo.seek(0) #a partir do índice 0
 conteudo = arquivo.read()
 print(conteudo)
@@ -8,3 +8,11 @@ quantidade_caracteres_arquivo = arquivo.tell()
 print(quantidade_caracteres_arquivo)
 
 arquivo.close()
+
+with open('exemplo.txt', 'a+') as arquivo:
+    arquivo.write('Quarta linha é feita com uso de gerenciamento de contexto\n'+\
+                  'Esta e a quinta linha e foi feita da mesma forma\n')
+
+    arquivo.seek(0)
+    conteudo = arquivo.read()
+    print(conteudo)
