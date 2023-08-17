1d278f3db0afdd9570fbfab0f8c889c945214560
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-17 08:51:52 -0300

    função para armazenar quantidade de caracteres dentro do arquivo e fecha-lo

--- aula_4-persistencia_em_arquivo/aula_pratica.py
+++ aula_4-persistencia_em_arquivo/aula_pratica.py
@@ -1,5 +1,10 @@
-arquivo = open('exemplo.txt', 'w+')
+arquivo = open('exemplo.txt', 'w+') #w+ cria novo arquivo ou o substitui
 arquivo.write('Ola Mundo!\nEsta e a segunda linha\nEsta e a terceira linha')
-arquivo.seek(0)
+arquivo.seek(0) #a partir do índice 0
 conteudo = arquivo.read()
 print(conteudo)
+
+quantidade_caracteres_arquivo = arquivo.tell()
+print(quantidade_caracteres_arquivo)
+
+arquivo.close()
