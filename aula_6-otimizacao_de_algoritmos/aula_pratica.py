8dc86d16cf939c6ba832537948b164d3a14c8edf
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-22 19:43:10 -0300

    gráfico

--- aula_6-otimizacao_de_algoritmos/aula_pratica.py
+++ aula_6-otimizacao_de_algoritmos/aula_pratica.py
@@ -2,14 +2,16 @@ import time
 import pandas as pd
 import seaborn as sns
 
-#tempo constante
-#realizar comparação com outros tipos de funções
+
+# tempo constante
+# realizar comparação com outros tipos de funções
 def take_first(my_list):
     return my_list[0]
 
-#é uma lista restrita com apenas três elementos
+
+# é uma lista restrita com apenas três elementos
 sort_list = [5, 15, 20]
-tic = time.perf_counter()  #inciando a contagem do tempo computacional
+tic = time.perf_counter()  # inciando a contagem do tempo computacional
 comando = take_first(sort_list)
 toc = time.perf_counter()
 
@@ -18,8 +20,8 @@ print(comando)
 tempo_computacional = toc - tic
 print(f'Tempo Computacional = {tempo_computacional}')
 
-#reformulando a lista - tamanho
-long_list = [15] * 10**8
+# reformulando a lista - tamanho
+long_list = [15] * 10 ** 8
 len_lista = len(long_list)
 print(f'Tamanho da lista = {len_lista}')
 
@@ -30,12 +32,12 @@ tempo_computacional = toc - tic
 print(f'Primeiro elemento = {comando2}')
 print(f'Tempo Computacional = {tempo_computacional}')
 
-lista_tamanho = [10**l for l in range(1,5)]
+lista_tamanho = [10 ** l for l in range(1, 5)]
 print(lista_tamanho)
 
 constante_tempo = []
 for l in lista_tamanho:
-    lista = [15]*l
+    lista = [15] * l
 
 tic = time.perf_counter()
 y = take_first(lista)
@@ -43,3 +45,9 @@ toc = time.perf_counter()
 
 constante_tempo.append(toc - tic)
 print(constante_tempo)
+
+# utlizando dataframe para vizualisação organizada
+constante_df = pd.DataFrame(list(zip(lista_tamanho, constante_tempo)), columns=['N', 'tempo'])
+print(constante_df)
+
+_=sns.lmplot(x='N', y='tempo', data=constante_df, ci=None)
