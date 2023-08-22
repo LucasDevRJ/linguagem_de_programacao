447d7c65a83474e064e4777bdd24073ad381f772
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-22 18:31:06 -0300

    contagem computacional

--- aula_6-otimizacao_de_algoritmos/aula_pratica.py
+++ aula_6-otimizacao_de_algoritmos/aula_pratica.py
@@ -9,4 +9,8 @@ def take_first(my_list):
 
 #é uma lista restrita com apenas três elementos
 sort_list = [5, 15, 20]
-tic = time.clock() #inciando a contagem do tempo computacional
+tic = time.perf_counter()  #inciando a contagem do tempo computacional
+comando = take_first(sort_list)
+toc = time.perf_counter()
+
+print(comando)
