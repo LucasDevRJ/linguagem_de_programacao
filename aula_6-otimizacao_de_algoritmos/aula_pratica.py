9e0f634472f573b20238f1e6a217eb8b4f0b2ccf
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-22 18:17:21 -0300

    criação de listas e inicialização da contagem computacional

---
+++ aula_6-otimizacao_de_algoritmos/aula_pratica.py
@@ -0,0 +1,12 @@
+import time
+import pandas as pd
+import seaborn as sns
+
+#tempo constante
+#realizar comparação com outros tipos de funções
+def take_first(my_list):
+    return my_list[0]
+
+#é uma lista restrita com apenas três elementos
+sort_list = [5, 15, 20]
+tic = time.clock() #inciando a contagem do tempo computacional
